80c0b579d0f9315764b581b27f4414db1b589991
afuwamusa <afuwamusa@gmail.com>
2019-02-18 18:21:52 +0300

    returning the combined length and adding comments, using functions and adding comments

--- fizz.py
+++ fizz.py
@@ -1,15 +1,24 @@
 # a function that takes two lists and returns the combined length using the module operator
 def fizzbuzz(listx, listy):
+#creating varriables and giving length to the lists in the function
   a = len(listx)
   b = len(listy)
+# combining the two lists
   comlen = a + b
+#creating a conditional statement to return fizz if divisible by 3
   if comlen % 3 == 0:
     return "fizz"
+#creating a conditional statement to return buzz if divisible by 5
   elif comlen % 5 == 0:
     return "buzz"
-  else:
-    if comlen % 5 and comlen % 3 == 0:
+#creating a conditional statement to return fizzbuzz if divisible by 5 and 3 
+  elif comlen % 5 and comlen % 3 == 0:
       return "fizzbuzz"
+#returning the combined length
+  else: 
+    return comlen
+  
+  
 print(fizzbuzz([1,2,3,4,5], [6,7,8,9]))
  
 
--- lists_b.py
+++ lists_b.py
@@ -1,18 +1,29 @@
-#a function that takes a string and returns a tuple
-string_list = input("enter a sentence: ").lower().split(' ')
-y = ''
+#creating a function that returns a string of vowels and the number of letters that appear more than once in a sentence.
+def vowels(string_listx):
+    #it's creating a variable that holds the sentence entered
+    string_list = string_listx.lower().split(' ')
+    #creating an empty string  to hold characters entered by the user
+    y = ''
 
-for word in string_list:
-    y += word
-vowels = ['a','e','i','o','u']
-vowels_in_string = [vowel for vowel in vowels if vowel in y]
-vowels_string = ""
-for vowel in vowels_in_string:
-    vowels_string =vowels_string + vowel
-t = []
-x = [t.append(letter) for letter in y if y.count(letter) > 1 and letter not in t]
-
-print(x)
-results = (vowels_string , len(t))
-print(results) 
+    for word in string_list:
+    #adding characters in the empty string created above.
+        y += word
+    #creating a list of vowels
+    vowels = ['a','e','i','o','u']
+    # using a list comprehation to retun vowels in string y
+    vowels_in_string = [vowel for vowel in vowels if vowel in y]
+    #creating an empty sring to hold the vowels in the sentence
+    vowels_string = ""
+    #looking through the vowels  in the list comprehation
+    for vowel in vowels_in_string:
+    #adding the vowel in the empty string
+        vowels_string =vowels_string + vowel
+    #creating an empty list to hold the duplicates
+    t = []
+    #adding all duplicates in the empty list that appear more than once and should not be repeated
+    x = [t.append(letter) for letter in y if y.count(letter) > 1 and letter not in t]
+    #returning the vowels and the number of duplicates in the sentence
+    results = (vowels_string , len(t))
+    return results
+print(vowels("type a sentence: ")) 
     
